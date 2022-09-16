3057038316227e605efe9e0f7fa10f0243fb8483
bharv <bharveytan@gmail.com>
2022-09-16 06:49:08 -0700

    code spacing, use if-then instead of &&

--- code/index.js
+++ code/index.js
@@ -3,8 +3,6 @@ const fetchCityData = async () => {
     return await response.json();
 }
 
-
-
 const main = async () => {
     let cityData = await fetchCityData();
 
@@ -15,7 +13,7 @@ const main = async () => {
         const target = e.target;
         indicator.style.width = `${target.clientWidth}px`;
         indicator.style.left = `${target.offsetLeft}px`;
-        menuItems.forEach( menuItem => {
+        menuItems.forEach(menuItem => {
             menuItem.classList.remove('active');
         });
         target.className = `${target.className} active`;
@@ -23,23 +21,22 @@ const main = async () => {
 
     const handleResize = () => {
         const activeElement = document.getElementsByClassName('active')[0];
-        activeElement && (indicator.style.width = `${activeElement.clientWidth}px`);
-        activeElement && (indicator.style.left = `${activeElement.offsetLeft}px`);
+        if (activeElement) {
+            indicator.style.width = `${activeElement.clientWidth}px`;
+            indicator.style.left = `${activeElement.offsetLeft}px`
+        }
     };
 
-    cityData.cities.forEach( city => {
+    cityData.cities.forEach(city => {
         const linkElement = document.createElement('a')
         linkElement.textContent = city.label;
         linkElement.className = 'menu-item';
         linkElement.href = '#';
         linkElement.addEventListener('click', handleClick);
-       document.getElementById('menu').appendChild(linkElement);
+        document.getElementById('menu').appendChild(linkElement);
     });
 
     window.addEventListener('resize', handleResize)
-
-
-
 };
 
 window.addEventListener('load', main);
